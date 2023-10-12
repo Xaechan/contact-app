f39f1e1416bc43628f5471adb5d15b1ae655cd1f
Magical Forest <linux@forestlinux.org>
2023-10-12 14:56:47 +0700

    contact-app

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import ContactApp from "./components/ContactApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ContactApp />
+    </React.StrictMode>
+);
